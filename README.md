e9f4420cfff0202ae6b57baba9ae6ce48832bcf2
Betül Altunel <altnbetul99@gmail.com>
2024-06-08 17:20:08 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# MI-POWER_Final_Case
-Mİ-POWER Women Empowerment Programı Final Case'i
+# MI-POWER Empowerment Programı Final Case: Kalp Hastalığı Veri Analizi Flask Uygulaması 
+
+Bu uygulamada, Kalp Hastalığı Tahmini veri seti kullanılarak yapılan
+bir veri analizi projesini ele almaktadır.
+Amaç, klinik değişkenlerle bireylerin kalp hastalığı riskini tahmin
+etmektir.
+
+## Uygulanan Veri Analiz Yöntemleri
+
+1-Kategorik ve Sayısal Değişkenlerin Belirlenmesi
+
+2-Eksik Değerlerin (Missing Values) Analizi
+
+3-Kategorik Değişken Analizi
+
+4-Sayısal Değişken Analizi
+
+5-Hedef Değişken Analizi (Kategorik&Sayısal)
+
+6-Aykırı Değerlerin Analizi ve IQR Yöntemi ile Tespiti
+
+7-Aykırı Değerler için Min-Max ölçeklendirmesi
+
+8-Encoding (Label Encoding)
+
+9-Korelasyon Analizi
+
+10-Feature Scaling
+
+11-Random Forest Classifier
+
+12-Confusion Matrix
+
+13-Sınıflandırma Parametreleri
+
+
+## Kurulum
+
+1. Depoyu klonlayın:
+     
+* git clone https://github.com/betulaltunl/MI-POWER-Final-Case.git
+ 
+
+2. Proje dizinine gidin:
+   
+* cd heart-disease-flask-app
+
+3. Sanal ortamı etkinleştirin:
+   
+    Windows için :
+
+    * .\env\Scripts\activate
+   
+    macOS ve Linux için:
+  
+    * source env/bin/activate
+  
+   
+4. Install the required dependencies:
+   
+* pip install -r requirements.txt
+
+## Kullanım
+
+1- Kalp hastalığı veri setini aşağıdaki linkden indirip bir CSV dosyası olarak kaydedin:
+
+[Heart Disease Veri Seti](https://www.kaggle.com/datasets/thedevastator/predicting-heart-disease-risk-using-clinical-var)
+
+2- Flask uygulamasını çalıştırın:
+
+* flask run
+
+3- Web tarayıcınızda görüntüleyin 
+
+4- Sağlanan formu kullanarak CSV veri dosyanızı yükleyin.
+
+## Proje Yapısı
+
+![image](https://github.com/betulaltunl/Heart-Disease-Prediction/assets/101793578/00be0b24-1e1d-4742-b1f5-96ff976b820c)
+
+
+- app.py: Ana Flask uygulama dosyası.
+
+- templates/: HTML şablonlarını içeren dizin.
+
+- index.html: Kullanıcıların veri setini yüklediği ana sayfa.
+
+- result.html: Analiz sonuçlarının görüntülendiği sayfa.
+
+- requirements.txt: Proje için gerekli Python paketlerinin listesi.
+
+- README.md: Bu readme dosyası.
+
+
+## Proje Ekran Görüntüleri
+
+
+![image](ss/1.png)
+![image](ss/2.png)
+![image](ss/3.png)
+
+
--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
-
 import matplotlib
 matplotlib.use('Agg')
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 import io
 import base64
 import matplotlib.pyplot as plt
@@ -16,26 +15,42 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 import plotly.io as pio
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 import os
-
-
-    
-
-    
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+def remove_index_column(df):
+    if 'index' in df.columns:
+        df.drop('index', axis=1, inplace=True)
+        print("'index' sütunu veri setinden silindi.")
+    else:
+        print("'index' sütunu veri setinde bulunamadı veya indeks sütunu zaten mevcut değil.")
+    return df
+
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+    return cat_cols, num_cols, cat_but_car, num_but_cat
+
 
 def get_head_and_tail(df):
     head_html = df.head().to_html(classes='table table-striped')
     tail_html = df.tail().to_html(classes='table table-striped')
     return head_html, tail_html
 
-
-def create_pie_chart(df):
-    counts = df["Heart Disease"].value_counts()
+def create_pie_chart(df, target_column):
+    counts = df[target_column].value_counts()
     total = counts.sum()
     fig = go.Figure(data=[go.Pie(
         labels=counts.index, 
@@ -44,19 +59,16 @@ def create_pie_chart(df):
         textinfo='label+percent',
         texttemplate='%{label}: %{percent:.1f} (%{value:d})'
     )])
-    fig.update_layout(title="Heart Disease Distribution")
+    fig.update_layout(title=f"{target_column} Distribution")
     return pio.to_html(fig, full_html=False)
 
-def create_boxplot(df):
-    fig = px.box(df, title="Boxplot of Variables")
+
+def create_boxplot(df, num_cols):
+    fig = px.box(df, y=num_cols, title="Boxplot of Variables")
     fig.update_layout(xaxis_title='Variables', yaxis_title='Values')
     return pio.to_html(fig, full_html=False)
 
-def create_unique_counts_plot(df):
-    unique_counts = df.nunique()
-    fig = px.bar(unique_counts, title='Her Sütundaki Benzersiz Değer Sayıları')
-    fig.update_layout(xaxis_title='Sütunlar', yaxis_title='Benzersiz Değer Sayısı')
-    return pio.to_html(fig, full_html=False)
+
 
 def create_missing_values_plot(df):
     missing_values_count = df.isnull().sum()
@@ -66,7 +78,6 @@ def create_missing_values_plot(df):
                      title='Missing Values Count', labels={'x': 'Columns', 'y': 'Missing Values Count'})
         return pio.to_html(fig, full_html=False)
     return '<p>No missing values found.</p>'  
-
 
 def create_correlation_heatmap(df, num_cols):
     num_df = df[num_cols]  
@@ -80,7 +91,6 @@ def create_correlation_heatmap(df, num_cols):
     ))
     fig.update_layout(title='Correlation Heatmap', xaxis=dict(title='Features'), yaxis=dict(title='Features'))
     return pio.to_html(fig, full_html=False)
-
 
 def create_cat_summary_plots(df, cat_cols):
     plots = []
@@ -103,7 +113,6 @@ def create_num_summary_plots(df, num_cols):
         plots.append(pio.to_html(fig_box, full_html=False))
     return plots
 
-
 def create_target_cat_plots(df, target, cat_cols):
     cat_cols.remove(target)  
     plots = []
@@ -112,7 +121,6 @@ def create_target_cat_plots(df, target, cat_cols):
         fig = px.bar(summary_df, x=col, y=target, title=f'{col} vs. {target}', labels={col: col, target: target})
         plots.append(pio.to_html(fig, full_html=False))
     return plots
-
 
 def create_target_num_plots(df, target, num_cols):
     plots = []
@@ -130,28 +138,29 @@ def create_outlier_plots(df, num_cols):
         IQR = Q3 - Q1
         lower_bound = Q1 - 1.5 * IQR
         upper_bound = Q3 + 1.5 * IQR
-        fig = px.box(df, y=col, title=f'{col} Aykırı Değer Analizi (IQR Method)')
+
+        # İlk kutu grafiği (aykırı değerler dahil)
+        fig = px.box(df, y=col, title=f'{col} Outlier Analysis (IQR Method)')
         fig.add_shape(type="line", x0=0, x1=1, y0=lower_bound, y1=lower_bound, line=dict(color="red", width=2))
         fig.add_shape(type="line", x0=0, x1=1, y0=upper_bound, y1=upper_bound, line=dict(color="red", width=2))
         plots.append(pio.to_html(fig, full_html=False))
+
+        # Aykırı değerleri min-max normalizasyonu ile dönüştür
+        df_normalized = df.copy()
+        scaler = MinMaxScaler()
+        df_normalized[col] = scaler.fit_transform(df_normalized[col].values.reshape(-1, 1))
+
+        # İkinci kutu grafiği (aykırı değerler dönüştürülmüş)
+        fig = px.box(df_normalized, y=col, title=f'{col} Outlier Analysis (IQR Method - Normalized)')
+        fig.add_shape(type="line", x0=0, x1=1, y0=scaler.transform([[lower_bound]])[0][0], 
+                     y1=scaler.transform([[lower_bound]])[0][0], line=dict(color="red", width=2))
+        fig.add_shape(type="line", x0=0, x1=1, y0=scaler.transform([[upper_bound]])[0][0], 
+                     y1=scaler.transform([[upper_bound]])[0][0], line=dict(color="red", width=2))
+
+        plots.append(pio.to_html(fig, full_html=False))
     return plots
 
-def grab_col_names(dataframe, cat_th=10, car_th=20):
-    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
-    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
-    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
-    cat_cols = cat_cols + num_but_cat
-    cat_cols = [col for col in cat_cols if col not in cat_but_car]
-    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
-    num_cols = [col for col in num_cols if col not in num_but_cat]
-    return cat_cols, num_cols, cat_but_car, num_but_cat
-
-
-
-
-
-
-
+# İNDEX.HTML SAYFASI İÇİN---------
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -161,87 +170,103 @@ def upload_file():
             file.save(filepath)
             return redirect(url_for('results', filename=file.filename))
     return render_template('index.html')
+#----------------------------------
 
 @app.route('/results/<filename>')
 def results(filename):
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     df = pd.read_csv(filepath)
-    df.drop(['index'], axis=1, inplace=True, errors='ignore')
-    label_encoder = LabelEncoder()
-    df['Heart Disease'] = label_encoder.fit_transform(df['Heart Disease'])
+
+     # 'index' sütununu sil
+    df = remove_index_column(df)
+    
+    # Eğer 'Heart Disease' kolonu yoksa, ilk sütunu hedef olarak kabul edebiliriz.
+    if 'Heart Disease' not in df.columns:
+        target_column = df.columns[0]
+    else:
+        target_column = 'Heart Disease'
+    
+    if df[target_column].dtype == 'O':
+        label_encoder = LabelEncoder()
+        df[target_column] = label_encoder.fit_transform(df[target_column])
     
     cat_cols, num_cols, cat_but_car, num_but_cat = grab_col_names(df)
     
     num_samples = df.shape[0]
     num_features = df.shape[1]
-    
 
-   
-    matplotlib.use('Agg')
-
-    pie_chart = create_pie_chart(df)
-    boxplot = create_boxplot(df)
-    unique_counts_plot = create_unique_counts_plot(df)
+    pie_chart = create_pie_chart(df, target_column)
+    boxplot = create_boxplot(df, num_cols)
     correlation_heatmap = create_correlation_heatmap(df, num_cols)
     cat_summary_plots = create_cat_summary_plots(df, cat_cols)
     num_summary_plots = create_num_summary_plots(df, num_cols)
-    target_cat_plots = create_target_cat_plots(df, 'Heart Disease', cat_cols)
-    target_num_plots = create_target_num_plots(df, 'Heart Disease', num_cols)
+    target_cat_plots = create_target_cat_plots(df, target_column, cat_cols)
+    target_num_plots = create_target_num_plots(df, target_column, num_cols)
     outlier_plots = create_outlier_plots(df, num_cols)
     missing_values_plot = create_missing_values_plot(df)  
     head_html, tail_html = get_head_and_tail(df)
 
-
- 
-    X = df.drop('Heart Disease', axis=1)  
-    y = df['Heart Disease']  
-    
+    X = df.drop(target_column, axis=1)  
+    y = df[target_column]  
     
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
     
-    
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-    
     
     model = RandomForestClassifier()
     model.fit(X_train, y_train)
-    
+
+    # Eğitim seti üzerinde tahmin yapma
+    y_train_pred = model.predict(X_train)
+
+    # Eğitim seti performansı
+    train_accuracy = accuracy_score(y_train, y_train_pred)
     
     accuracy = model.score(X_test, y_test)
     
-   
     y_pred = model.predict(X_test)
     
-    
+    # Confusion Matrix oluşturma
     cm = confusion_matrix(y_test, y_pred)
-    
-    
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(cm, annot=True, cmap='Blues', fmt='g', cbar=False)
-    plt.title('Confusion Matrix')
-    plt.xlabel('Predicted Labels')
-    plt.ylabel('True Labels')
-    
-    
-    buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    image_png = buffer.getvalue()
-    buffer.close()
-    
-    
-    graphic = base64.b64encode(image_png)
-    graphic = graphic.decode('utf-8')
-    
-    plt.close()
-    
-    return render_template('result.html', num_samples=num_samples, num_features=num_features, cat_cols=cat_cols, 
-                           num_cols=num_cols, cat_but_car=cat_but_car, num_but_cat=num_but_cat, pie_chart=pie_chart, 
-                           boxplot=boxplot, unique_counts_plot=unique_counts_plot, correlation_heatmap=correlation_heatmap, 
-                           cat_summary_plots=cat_summary_plots, num_summary_plots=num_summary_plots, 
-                           target_cat_plots=target_cat_plots, target_num_plots=target_num_plots, outlier_plots=outlier_plots,
-                           missing_values_plot=missing_values_plot, head_html=head_html, tail_html=tail_html, accuracy=accuracy, graphic=graphic)
+    cm_fig = plt.figure(figsize=(16, 12))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=label_encoder.classes_, yticklabels=label_encoder.classes_)
 
-if __name__ == "__main__":
+    plt.title('Confusion Matrix')
+    plt.ylabel('Actual', fontsize=16)
+    plt.xlabel('Predicted',fontsize=16)
+    cm_buf = io.BytesIO()
+    cm_fig.savefig(cm_buf, format='png')
+    cm_buf.seek(0)
+    cm_img_base64 = base64.b64encode(cm_buf.getvalue()).decode('utf-8')
+    plt.close(cm_fig)
+
+    report = classification_report(y_test, y_pred)
+    
+    return render_template('result.html', 
+                           num_samples=num_samples,
+                           num_features=num_features,
+                           num_cols=num_cols, 
+                           cat_cols=cat_cols,
+                           cat_but_car=cat_but_car, 
+                           num_but_cat=num_but_cat,
+                           pie_chart=pie_chart,
+                           boxplot=boxplot,
+                           correlation_heatmap=correlation_heatmap,
+                           cat_summary_plots=cat_summary_plots,
+                           num_summary_plots=num_summary_plots,
+                           target_cat_plots=target_cat_plots,
+                           target_num_plots=target_num_plots,
+                           outlier_plots=outlier_plots,
+                           missing_values_plot=missing_values_plot,
+                           head_html=head_html,
+                           tail_html=tail_html,
+                           train_accuracy=train_accuracy,
+                           accuracy=accuracy,
+                           confusion_matrix=cm_img_base64,
+                           classification_report=report)
+
+if __name__ == '__main__':
     app.run(debug=True)
+    
+                    
